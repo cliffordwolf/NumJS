5859dfd575fa9af609d728566d229849972036f7
Clifford Wolf <clifford@clifford.at>
2011-09-11 22:18:49 +0200

    Implemented .rref()

--- NumJS.MatRREF.js
+++ NumJS.MatRREF.js
@@ -29,8 +29,41 @@ NumJS.GenericMatrix.prototype.rref = function()
 	var R = this.copy();
 	var pivcols = new Array();
 
-	// TBD!
-	window.alert("Matrix.rref() isn't implemented yet!");
+	var rank = 0;
+	for (var j = 0; j < R.cols && rank < R.rows; j++)
+	{
+		var i = rank;
+		var i_abs = NumJS.ABS(R.get(i, j));
+		for (var k = rank+1; k < R.rows; k++) {
+			var k_abs = NumJS.ABS(R.get(k, j));
+			if (k_abs < i_abs)
+				i = k, i_abs = k_abs;
+		}
+
+		if (i_abs == 0)
+			continue;
+
+		var pivot = R.get(i, j);
+		for (var l = j; l < R.cols; l++) {
+			var val = l != j ? NumJS.DIV(R.get(i, l), pivot) : 1;
+			if (i != rank)
+				R.set(i, l, R.get(rank, l));
+			R.set(rank, l, val);
+		}
+
+		for (i = 0; i < R.rows; i++)
+		{
+			if (i == rank)
+				continue;
+			var factor = R.get(i, j);
+			for (var l = j+1; l < R.cols; l++)
+				R.set(i, l, NumJS.SUB(R.get(i, l), NumJS.MUL(R.get(rank, l), factor)));
+			R.set(i, j, 0);
+		}
+
+		pivcols.push(j);
+		rank++;
+	}
 
 	R.pivcols = pivcols;
 	this.cache["rref"] = R;
